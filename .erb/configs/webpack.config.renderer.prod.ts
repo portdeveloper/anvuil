f6a98f5239d7b08f7e77651529bd21d607c3ef20
port <schoolreef2@protonmail.com>
2023-07-10 00:14:04 +0300

    Add tailwindcss, fix errors in renderer.dev.ts

--- .erb/configs/webpack.config.renderer.prod.ts
+++ .erb/configs/webpack.config.renderer.prod.ts
@@ -56,6 +56,23 @@ const configuration: webpack.Configuration = {
       },
       {
         test: /\.s?(a|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [require('tailwindcss'), require('autoprefixer')],
+              },
+            },
+          },
+        ],
+        exclude: /\.module\.s?(c|a)ss$/,
+      },
+      {
+        test: /\.s?(a|c)ss$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
         exclude: /\.module\.s?(c|a)ss$/,
       },
--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,40 +1,12 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
-import './App.css';
+import 'tailwindcss/tailwind.css';
 
 function Hello() {
   return (
-    <div>
-      <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200 text-red-900">
+      <p className="text-5xl font-bold mb-4">Hello Electron!</p>
+      <p className="text-2xl">furkan</p>
+      <p className="text-2xl">berk</p>
     </div>
   );
 }
