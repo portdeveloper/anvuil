007f97dd5cc65f267460668c472b0167b71eaeff
port <schoolreef2@protonmail.com>
2023-07-10 21:39:00 +0300

    Extract Home to its own page

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,72 +1,8 @@
 import { MemoryRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import 'tailwindcss/tailwind.css';
 import { useEffect, useState } from 'react';
-import LogsWindow from './components/LogsWindow';
-
-function Hello() {
-  const [directory, setDirectory] = useState(null);
-  const [anvilParams, setAnvilParams] = useState('');
-
-  const selectDirectory = async () => {
-    const result = await window.electron.ipcRenderer.invoke(
-      'get-directory-path'
-    );
-    setDirectory(result.filePaths[0]);
-  };
-
-  const startAnvil = async () => {
-    if (!directory) {
-      alert('Please select a directory first.');
-      return;
-    }
-    await window.electron.ipcRenderer.invoke(
-      'start-anvil',
-      directory,
-      anvilParams
-    );
-  };
-
-  const killAnvil = async () => {
-    await window.electron.ipcRenderer.invoke('kill-anvil');
-  };
-
-  useEffect(() => {
-    const fetchDirectory = async () => {
-      const dir = await window.electron.ipcRenderer.invoke(
-        'get-saved-directory'
-      );
-      setDirectory(dir);
-    };
-
-    fetchDirectory();
-  }, []);
-
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200 text-blue-500">
-      <p className="text-5xl font-bold mb-4">Hello Electron!</p>
-      <div className="flex gap-10 text-red-500">
-        <button type="button" onClick={selectDirectory}>
-          Select Project Directory
-        </button>
-        <div className="flex flex-col">
-          <input
-            type="text"
-            value={anvilParams}
-            onChange={(e) => setAnvilParams(e.target.value)}
-            placeholder="Enter Anvil parameters"
-          />
-          <button type="button" onClick={startAnvil}>
-            Start Anvil
-          </button>
-        </div>
-
-        <button type="button" onClick={killAnvil}>
-          Kill Anvil
-        </button>
-      </div>
-    </div>
-  );
-}
+import LogsWindow from './pages/LogsWindow';
+import Home from './pages/Home';
 
 export default function App() {
   const [output, setOutput] = useState('');
@@ -99,7 +35,7 @@ export default function App() {
         </nav>
 
         <Routes>
-          <Route path="/" element={<Hello />} />
+          <Route path="/" element={<Home />} />
           <Route path="/logs-window" element={<LogsWindow output={output} />} />
         </Routes>
       </div>
---
+++ src/renderer/pages/Home.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from 'react';
+
+export default function Home() {
+  const [directory, setDirectory] = useState(null);
+  const [anvilParams, setAnvilParams] = useState('');
+
+  const selectDirectory = async () => {
+    const result = await window.electron.ipcRenderer.invoke(
+      'get-directory-path'
+    );
+    setDirectory(result.filePaths[0]);
+  };
+
+  const startAnvil = async () => {
+    if (!directory) {
+      alert('Please select a directory first.');
+      return;
+    }
+    await window.electron.ipcRenderer.invoke(
+      'start-anvil',
+      directory,
+      anvilParams
+    );
+  };
+
+  const killAnvil = async () => {
+    await window.electron.ipcRenderer.invoke('kill-anvil');
+  };
+
+  useEffect(() => {
+    const fetchDirectory = async () => {
+      const dir = await window.electron.ipcRenderer.invoke(
+        'get-saved-directory'
+      );
+      setDirectory(dir);
+    };
+
+    fetchDirectory();
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-200 text-blue-500">
+      <p className="text-5xl font-bold mb-4">Hello Electron!</p>
+      <div className="flex gap-10 text-red-500">
+        <button type="button" onClick={selectDirectory}>
+          Select Project Directory
+        </button>
+        <div className="flex flex-col">
+          <input
+            type="text"
+            value={anvilParams}
+            onChange={(e) => setAnvilParams(e.target.value)}
+            placeholder="Enter Anvil parameters"
+          />
+          <button type="button" onClick={startAnvil}>
+            Start Anvil
+          </button>
+        </div>
+
+        <button type="button" onClick={killAnvil}>
+          Kill Anvil
+        </button>
+      </div>
+    </div>
+  );
+}
