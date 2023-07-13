e3d2b21105796c63ba0f325c31ed2679c548bd8d
port <schoolreef2@protonmail.com>
2023-07-13 21:09:32 +0300

    Able to get the latest block

--- src/renderer/pages/Accounts.tsx
+++ src/renderer/pages/Accounts.tsx
@@ -9,7 +9,7 @@ export const Accounts = ({ accounts }: AccountsProps) => {
   return (
     <div className="h-full flex items-center justify-center p-5 bg-gray-900 text-white">
       {accounts.length > 0 ? (
-        <div className="flex flex-col gap-2 w-full">
+        <div className="flex flex-col gap-2 w-full max-h-[600px] overflow-auto">
           {accounts.map((account) => (
             <AddressBlock key={account} address={account} />
           ))}
--- src/renderer/pages/Blocks.tsx
+++ src/renderer/pages/Blocks.tsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react';
+import { anvilClient } from 'renderer/client';
+
 export const Blocks = () => {
+  const [blocks, setBlocks] = useState<any[]>([]); // @todo type
+  const [blockNumber, setBlockNumber] = useState<BigInt | null>(null);
+
+  useEffect(() => {
+    const unwatch = anvilClient.watchBlocks({
+      emitOnBegin: true,
+      onBlock: (block) => {
+        console.log(block);
+        setBlockNumber(block.number);
+        setBlocks((prevBlocks) => [...prevBlocks, block]);
+      },
+    });
+
+    return () => {
+      unwatch();
+    };
+  }, []);
+
   return (
-    <div className="flex-grow h-full flex items-center justify-center p-10 bg-gray-900 text-white">
-      <h1>blocks</h1>
+    <div className="flex flex-col flex-grow h-full items-center justify-center p-10 bg-gray-900 text-white">
+      <h1>Block number: {blockNumber?.toString()}</h1>
+      {blocks.map((block) => (
+        <div>{block.transactions}</div>
+      ))}
     </div>
   );
 };
