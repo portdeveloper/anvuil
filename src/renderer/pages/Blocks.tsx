770052d365430bca983670ae0fbd4ac63b252453
port <schoolreef2@protonmail.com>
2023-07-14 22:36:36 +0300

    Change how blocks are shown

--- src/renderer/pages/Blocks.tsx
+++ src/renderer/pages/Blocks.tsx
@@ -3,12 +3,11 @@ import { anvilClient } from 'renderer/client';
 import { Block } from 'viem';
 
 export const Blocks = () => {
-  const [blocks, setBlocks] = useState<Block[]>([]); // @todo type
+  const [blocks, setBlocks] = useState<Block[]>([]);
   const [blockNumber, setBlockNumber] = useState<bigint>(0n);
 
   useEffect(() => {
     const unwatch = anvilClient.watchBlocks({
-      emitOnBegin: true,
       onBlock: (block) => {
         console.log(block);
         setBlockNumber(block.number || 0n);
@@ -41,21 +40,24 @@ export const Blocks = () => {
     <div className="flex flex-col items-center p-5 bg-gray-900 text-white overflow-hidden h-full">
       <h1>Block number: {blockNumber?.toString()}</h1>
       <div className="overflow-auto flex flex-col gap-4 w-full h-0 flex-grow">
-        {blocks.map((block) => (
-          <div className="flex flex-col bg-slate-500 p-4">
-            <p key={block.hash}>Block hash: {block.hash}</p>
-            <p>Gas limit: {Number(block.gasLimit)}</p>
-            <p>Gas used: {Number(block.gasUsed)}</p>
-            <div>
-              Transactions:
-              {block.transactions.map((tx) => (
-                <div>
-                  <p>{tx.toString()}</p>
-                </div>
-              ))}
+        {blocks
+          .slice()
+          .reverse()
+          .map((block) => (
+            <div className="flex flex-col bg-slate-500 p-4">
+              <p key={block.hash}>Block hash: {block.hash}</p>
+              <p>Gas limit: {Number(block.gasLimit)}</p>
+              <p>Gas used: {Number(block.gasUsed)}</p>
+              <div>
+                Transactions:
+                {block.transactions.map((tx) => (
+                  <div>
+                    <p>{tx.toString()}</p>
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
