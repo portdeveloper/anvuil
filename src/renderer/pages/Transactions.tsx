370eaca1316c677aa5f82fa4938b6ad95893ad7b
port <schoolreef2@protonmail.com>
2023-07-15 16:40:59 +0300

    Implement Transactions page

--- src/renderer/pages/Transactions.tsx
+++ src/renderer/pages/Transactions.tsx
@@ -1,43 +1,19 @@
-import { useEffect, useState } from 'react';
-import { anvilClient } from 'renderer/client';
-import { Hash } from 'viem';
+import { useContext } from 'react';
+import { BlocksContext } from '../BlocksContext';
 
 export const Transactions = () => {
-  const [hashes, setHashes] = useState<Hash[]>([]);
-  const [txReceipts, setTxReceipts] = useState<any[]>([]); // @todo type
-
-  useEffect(() => {
-    const unwatch = anvilClient.watchPendingTransactions({
-      onTransactions: (hashes) => {
-        setHashes(hashes);
-      },
-    });
-
-    return () => {
-      unwatch();
-    };
-  }, []);
-
-  useEffect(() => {
-    const fetchTransactions = async () => {
-      const fetchedTransactions = await Promise.all(
-        hashes.map((tx) => anvilClient.getTransaction({ hash: tx }))
-      );
-
-      setTxReceipts((prev) => [...prev, ...fetchedTransactions]);
-    };
-    if (hashes.length) {
-      fetchTransactions();
-    }
-  }, [hashes]);
+  const { transactions } = useContext(BlocksContext);
 
   return (
     <div className="flex flex-col items-center p-5 bg-gray-900 text-white overflow-hidden h-full">
       <h1>transactions</h1>
       <div className="overflow-auto flex flex-col gap-4 w-full h-0 flex-grow">
-        {txReceipts.map((tx) => (
-          <div>
-            <p>{tx.hash}</p>
+        {transactions.map((tx) => (
+          <div className="flex flex-col bg-slate-500 p-4">
+            <p>tx hash: {tx.hash}</p>
+            <p>tx from: {tx.from}</p>
+            <p>tx to: {tx.to}</p>
+            <p>tx value: {tx.value}</p>
           </div>
         ))}
       </div>
