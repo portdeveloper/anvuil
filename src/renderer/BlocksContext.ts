2d04a45ed1e5e7ceb121a1f12265939c0a96c279
port <schoolreef2@protonmail.com>
2023-07-15 12:58:36 +0300

    Use react context to pass blocks data

---
+++ src/renderer/BlocksContext.ts
@@ -0,0 +1,16 @@
+import { createContext } from 'react';
+import { Block } from 'viem';
+
+type BlocksContextType = {
+  blocks: Block[];
+  blockNumber: number;
+  transactions: any[]; // @todo type
+  reset: () => void;
+};
+
+export const BlocksContext = createContext<BlocksContextType>({
+  blocks: [],
+  blockNumber: 0,
+  transactions: [],
+  reset: () => {},
+});
---
+++ src/renderer/BlocksProvider.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useMemo, useState } from 'react';
+import { BlocksContext } from './BlocksContext';
+import { anvilClient } from './client';
+import { Block } from 'viem';
+
+export const BlocksProvider = ({ children }: { children: React.ReactNode }) => {
+  const [blocks, setBlocks] = useState<Block[]>([]);
+  const [blockNumber, setBlockNumber] = useState<number>(0);
+  const [transactions, setTransactions] = useState<any[]>([]);
+
+  const resetBlocksContext = () => {
+    setBlocks([]);
+    setBlockNumber(0);
+    setTransactions([]);
+    console.log('ATTEMPTING RESET');
+  };
+
+  const value = useMemo(
+    () => ({ blocks, blockNumber, transactions, reset: resetBlocksContext }),
+    [blocks, blockNumber, transactions]
+  );
+
+  useEffect(() => {
+    const unwatch = anvilClient.watchBlocks({
+      onBlock: (block) => {
+        console.log(block);
+        setBlockNumber(Number(block.number));
+        console.log(block.number);
+        setBlocks((prev) => [...prev, block]);
+      },
+      onError: (error) => console.log(error),
+    });
+    return () => {
+      unwatch();
+    };
+  }, []);
+  console.log('useeffect blocknum', blockNumber);
+
+  return (
+    <BlocksContext.Provider value={value}>{children}</BlocksContext.Provider>
+  );
+};
