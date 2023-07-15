106de37dc50a3a781e6fd34fa4aeda2e0c3f59a4
port <schoolreef2@protonmail.com>
2023-07-15 12:58:16 +0300

    Create InfoBar component

---
+++ src/renderer/components/InfoBar.tsx
@@ -0,0 +1,16 @@
+import { useContext } from 'react';
+import { BlocksContext } from 'renderer/BlocksContext';
+
+export const InfoBar = () => {
+  const { blockNumber } = useContext(BlocksContext);
+  console.log(blockNumber);
+
+  return (
+    <div className="flex gap-10 bg-green-400">
+      <div>Current block: {blockNumber}</div>
+      <div>Mining status:</div>
+      <div>other info about current state of local node</div>
+      <div>searchbar?</div>
+    </div>
+  );
+};
