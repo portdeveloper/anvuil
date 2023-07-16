b0e3cead7e638ea0e8c66af6ed92883b95a70636
port <schoolreef2@protonmail.com>
2023-07-16 10:39:46 +0300

    Add automine status and mine button

--- src/renderer/components/InfoBar.tsx
+++ src/renderer/components/InfoBar.tsx
@@ -3,7 +3,8 @@ import { BlocksContext } from 'renderer/BlocksContext';
 import { anvilClient } from 'renderer/client';
 
 export const InfoBar = () => {
-  const [autoMining, setAutoMining] = useState<boolean>(false); // @todo type
+  const [autoMining, setAutoMining] = useState<boolean>(false);
+  const [highlight, setHighlight] = useState(false); // state to control highlight
 
   const { blockNumber } = useContext(BlocksContext);
 
@@ -16,15 +17,53 @@ export const InfoBar = () => {
     }
   };
 
+  const toggleAutomine = async () => {
+    try {
+      await anvilClient.setAutomine(!autoMining);
+      fetchAutomineStatus();
+    } catch (error) {
+      console.error('Failed to set automine status: ', error);
+    }
+  };
+
   useEffect(() => {
     fetchAutomineStatus();
   }, []);
 
+  const mineBlock = async () => {
+    try {
+      await anvilClient.mine({
+        blocks: 1,
+      });
+    } catch (error) {
+      console.error('Failed to mine block: ', error);
+    }
+  };
+
+  // useEffect to listen to blockNumber changes
+  useEffect(() => {
+    setHighlight(true);
+    const timer = setTimeout(() => {
+      setHighlight(false);
+    }, 300); // change color for 500ms
+    return () => clearTimeout(timer);
+  }, [blockNumber]);
+
   return (
     <div className="flex gap-10 bg-green-400">
-      <div>Current block: {blockNumber}</div>
-      <div>AutoMining status: {autoMining ? 'true' : 'false'}</div>
-      <div>other info about current state of local node</div>
+      <div>
+        Current block:{' '}
+        <span className={` ${highlight ? 'animate-pulse bg-pink-300' : ''}`}>
+          {blockNumber}
+        </span>
+      </div>
+      <button type="button" className="bg-pink-300" onClick={toggleAutomine}>
+        AutoMining = {autoMining ? 'ON' : 'OFF'}
+      </button>
+      <button type="button" className="bg-pink-300" onClick={() => mineBlock()}>
+        Mine a block
+      </button>
+
       <div>searchbar?</div>
     </div>
   );
