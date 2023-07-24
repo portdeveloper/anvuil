dcad06c7a6709bc8c90aeb2ce51af7add698f2c9
port <schoolreef2@protonmail.com>
2023-07-24 11:26:39 +0300

    Implement Drop button in Mempool.tsx

--- src/renderer/pages/Mempool.tsx
+++ src/renderer/pages/Mempool.tsx
@@ -5,12 +5,20 @@ import { AddressComp, HashComp } from 'renderer/components/';
 
 export const Mempool = () => {
   const [mempool, setMempool] = useState({ pending: {}, queued: {} });
+  const [updateInterval, setUpdateInterval] = useState<number>(5000); // default to updating every 5 seconds
+  const [triggerFetch, setTriggerFetch] = useState(0);
 
   useEffect(() => {
     const fetchMempool = async () => {
       try {
         const memorypool = await anvilClient.getTxpoolContent();
-        console.log('⚠️⚠️⚠️ Mempool is fetched inside Mempool.tsx');
+        console.log(
+          '⚠️⚠️⚠️ Mempool is fetched inside Mempool.tsx',
+          'memorypool:',
+          memorypool,
+          'mempool:',
+          mempool
+        );
 
         setMempool(memorypool);
       } catch (error) {
@@ -18,10 +26,12 @@ export const Mempool = () => {
       }
     };
 
+    console.log('⚠️⚠️⚠️ Mempool.tsx is called', mempool);
+
     fetchMempool();
-    const intervalId = setInterval(fetchMempool, 5000);
+    const intervalId = setInterval(fetchMempool, updateInterval);
     return () => clearInterval(intervalId);
-  }, []);
+  }, [triggerFetch]);
 
   const pending: { [address: string]: { [nonce: string]: Transaction } } =
     mempool.pending as any;
@@ -31,9 +41,35 @@ export const Mempool = () => {
   return (
     <div className="flex h-full">
       <div className="px-3 py-2 flex flex-col gap-7 bg-secondary w-1/5">
-        {/* Here you can add whatever controls you want, like in Blocks.tsx */}
+        <div className="flex flex-col gap-1">
+          <p>Set update interval (s)</p>
+          <input
+            type="range"
+            min={1000}
+            max={10000}
+            step="1000"
+            value={updateInterval}
+            onChange={(e) => {
+              setUpdateInterval(Number(e.target.value));
+              setTriggerFetch((prev) => prev + 1);
+            }}
+            className="range range-xs"
+          />
+          <div className="w-full flex justify-between text-xs">
+            <span>1</span>
+            <span>2</span>
+            <span>3</span>
+            <span>4</span>
+            <span>5</span>
+            <span>6</span>
+            <span>7</span>
+            <span>8</span>
+            <span>9</span>
+            <span>10</span>
+          </div>
+        </div>
       </div>
-      <div className="px-5 w-full overflow-y-auto scrollbar-thin scrollbar-thumb-secondary">
+      <div className="px-5 py-2 w-full overflow-y-auto scrollbar-thin scrollbar-thumb-secondary">
         <h1 className="text-center text-xl">Pending Transactions</h1>
         <table className="table w-full table-compact">
           <thead>
@@ -42,7 +78,7 @@ export const Mempool = () => {
               <th>From</th>
               <th>To</th>
               <th>Value</th>
-              <th>Nonce</th>
+              <th>Drop Transaction</th>
             </tr>
           </thead>
           <tbody>
@@ -56,10 +92,26 @@ export const Mempool = () => {
                     <AddressComp address={tx.from} />
                   </td>
                   <td>
-                    <AddressComp address={tx.to} />
+                    {tx.to === null ? (
+                      <span className="text-xs">Contract Creation</span>
+                    ) : (
+                      <AddressComp address={tx.to} />
+                    )}
                   </td>
                   <td>{Number(tx.value)}</td>
-                  <td>{nonce}</td>
+                  <td>
+                    <button
+                      type="button"
+                      className="btn btn-error btn-xs"
+                      onClick={() => {
+                        anvilClient.dropTransaction({ hash: tx.hash });
+                        console.log('⚠️⚠️⚠️ Transaction dropped', tx.hash);
+                        setTriggerFetch((prev) => prev + 1);
+                      }}
+                    >
+                      Drop
+                    </button>
+                  </td>
                 </tr>
               ))
             )}
