99934ffe04b589e898d3a4e30414384d7ff75c83
port <schoolreef2@protonmail.com>
2023-07-25 13:03:52 +0300

    Add update interval to Events.tsx

--- src/renderer/pages/Events.tsx
+++ src/renderer/pages/Events.tsx
@@ -5,6 +5,7 @@ import { Log } from 'viem';
 
 export const Events = () => {
   const [logs, setLogs] = useState<Log[]>([]);
+  const [updateInterval, setUpdateInterval] = useState<number>(5000); // default to updating every 5 seconds
 
   useEffect(() => {
     const fetchLogs = async () => {
@@ -22,17 +23,41 @@ export const Events = () => {
 
     fetchLogs();
 
-    const intervalId = setInterval(fetchLogs, 5000);
+    const intervalId = setInterval(fetchLogs, updateInterval);
 
     return () => {
       clearInterval(intervalId);
     };
-  }, []);
+  }, [updateInterval]);
 
   return (
     <div className="flex h-full">
-      <div className="px-3 py-2 flex flex-col gap-7 bg-secondary w-1/5">
-        {/* Add controls here, if required */}
+      <div className="px-3 py-2 flex flex-col gap-7 bg-secondary w-1/5 justify-between">
+        <div></div>
+        <div className="flex flex-col gap-1 mb-2">
+          <p>Set update interval (s)</p>
+          <input
+            type="range"
+            min={1000}
+            max={10000}
+            step="1000"
+            value={updateInterval}
+            onChange={(e) => setUpdateInterval(Number(e.target.value))}
+            className="range range-xs"
+          />
+          <div className="w-full flex justify-between text-xs">
+            <span>1</span>
+            <span>2</span>
+            <span>3</span>
+            <span>4</span>
+            <span>5</span>
+            <span>6</span>
+            <span>7</span>
+            <span>8</span>
+            <span>9</span>
+            <span>10</span>
+          </div>
+        </div>
       </div>
       <div className="px-5 w-full overflow-y-auto scrollbar-thin scrollbar-thumb-secondary">
         <table className="table w-full table-compact">
